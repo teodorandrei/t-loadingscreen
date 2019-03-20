8b4452f522a8a2872f171a8038a1bca2cd9c22b2
teodorandrei <30527339+teodorandrei@users.noreply.github.com>
2019-03-20 19:55:28 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+files {
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'lalala.mp3',
+    'demo.js',
+    'jquery.particleground.js',
+    'jquery.particleground.min.js',
+    'jquery-1.11.1.min.js',
+    'Discord.png',
+    'Logo.png'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
---
+++ demo.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
